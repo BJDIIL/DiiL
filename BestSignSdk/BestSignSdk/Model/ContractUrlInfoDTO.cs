1100aba138f945a3fa212c91b9248acd8dbcb4eb
刘帅 <xiaobudian@users.noreply.github.com>
2018-05-18 18:06:02 +0800

    删除多余的文件

--- BestSignSdk/BestSignSdk/Model/ContractUrlInfoDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DiiL.Sdk.BestSignSdk.Model
-{
-    public class ContractUrlInfoDTO
-    {
-        public string shortUrl { get; set; }
-        public string longUrl { get; set; }
-    }
-}
--- BestSignSdk/BestSignSdk/Model/TemplateContractIdDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DiiL.Sdk.BestSignSdk.Model
-{
-    public class TemplateContractIdDTO
-    {
-        public string contractId { get; set; }
-    }
-}
