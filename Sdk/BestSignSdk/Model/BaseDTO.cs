e52ed2a1101c558e05f1634f8abac81f2ec4b3f8
刘帅 <xiaobudian@users.noreply.github.com>
2018-05-18 18:01:05 +0800

    整理文件夹

---
+++ Sdk/BestSignSdk/Model/BaseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DiiL.Sdk.BestSignSdk.Model
+{
+    public class BaseDTO<T> where T : class
+    {
+        public string errno { get; set; }
+        public T data { get; set; }
+        public string errmsg { get; set; }
+
+    }
+}
